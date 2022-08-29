9ce38710434bcbf1434763e5d232d889ef9a1843
Alejandro Haller <lucahhaller@gmail.com>
2022-08-29 17:54:37 -0300

    deploy2

--- api/src/app.js
+++ api/src/app.js
@@ -1,29 +1,52 @@
 const express = require('express');
-const cors = require('cors')
+
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
 
 require('./db.js');
-
 const server = express();
+const cors = require('cors');
 
 server.name = 'API';
 
+server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
+	//Si en la instruccion de abajo sacamos * y ponemos la url de la pagina sirve para produccion
+	res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+	res.header('Access-Control-Allow-Credentials', 'true');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	res.header(
+		'Access-Control-Allow-Methods',
+		'GET, POST, OPTIONS, PUT, DELETE'
+	);
+	next();
 });
+// const server = express();
 
-server.use(cors({origin:'http://localhost:3006' ,credentials:true}));
+// server.name = 'API';
+
+// server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+// server.use(bodyParser.json({ limit: '50mb' }));
+// server.use(cookieParser());
+// server.use(morgan('dev'));
+// server.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   next();
+// });
+
+// server.use(cors({origin:'http://localhost:3006' ,credentials:true}));
 server.use('/', routes);
 
 // Error catching endware.
